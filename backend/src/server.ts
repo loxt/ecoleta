504841428189f383bf60f84f0886e73ac4544dff
Emannuel Matos <emannuelloxt@hotmail.com>
2020-06-03 14:01:53 -0400

    :sparkles: feat: create migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.json({ msg: 'Hello World!' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(8080);
