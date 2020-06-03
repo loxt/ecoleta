1ebcab9067d0b9474c0edd23e96186a4f701ef1d
Emannuel Matos <emannuelloxt@hotmail.com>
2020-06-03 18:37:16 -0400

    :sparkles: feat: CRUD and CORS

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, {Request} from 'express';
+import knex from './database/connection';
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ msg: 'Hello World!' });
-});
+routes.get('/items', new ItemsController().index);
+
+routes.post('/points', new PointsController().create)
+routes.get('/points', new PointsController().index)
+routes.get('/points/:id', new PointsController().show)
 
 export default routes;
